dadb0a1efb0173e9a16a5e634dbbf43eb9ed265d
Cam Beatty <beatty2121@gmail.com>
2022-09-15 23:01:40 -0500

    WIP styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://cambeattydev.github.io/',
-	integrations: [mdx(), sitemap()],
+  site: "https://cambeattydev.github.io/",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({ config: { applyBaseStyles: false } }),
+  ],
 });
